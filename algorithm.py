cc8320ac5f1a9c176e8d03e20b7174e601518b39
Matthew <mattyg@live.unc.edu>
2020-06-25 11:28:42 -0400

    Finished extended Euclidean algorithm

--- algorithm.py
+++ algorithm.py
@@ -1,4 +1,4 @@
-from qiskit import *
+# from qiskit import *
 
 # In our final Jupyter notebook we won't have to
 # break things up functionally like this, but it
@@ -8,7 +8,19 @@ from qiskit import *
 # (the number y such that xy = 1 (mod N)) using
 # the extended Euclidean algorithm.
 def invert(x, N):
-    return 1
+    q = [0, 0]
+    r = [N, x]
+    t = [0, 1]
+
+    while r[-1] != 0:
+        q.append(r[-2]//r[-1])
+        r.append(r[-2] - (q[-1]*r[-1]))
+        t.append(t[-2] - (q[-1]*t[-1]))
+    
+    if r[-2] != 1:
+        raise Exception
+    
+    return t[-2] % N
 
 # b is some power of a, and the oracle outputs m,
 # where b = a^m (mod N) with >50% probability.
