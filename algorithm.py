e4fa0c675266245814880f4219268561ceb15be2
Matthew <mattyg@live.unc.edu>
2020-06-25 15:53:29 -0400

    got a decent part of step 1 done (I think)

--- algorithm.py
+++ algorithm.py
@@ -1,4 +1,5 @@
-from qiskit import *
+from qiskit import QuantumCircuit
+from qiskit.circuit.library import QFT
 import math
 
 # In our final Jupyter notebook we won't have to
@@ -27,20 +28,30 @@ def invert(x, N):
 # where b = a^m (mod N) with >50% probability.
 # (this is where our main algorithm goes)
 def oracle(a, b, N):
-    #Find number of bits(n) needed to store a value from 0 to N-1
-    #and initialize 2 quantum registers of size n
-    n = math.floor(math.log(N)/math.log(2)) + 1
-    qc = QuantumCircuit(QuantumRegister(n, 'qr0'), QuantumRegister(n, 'qr1'))
+    # Find number of bits(n) needed to store a value from 0 to N-1
+    # and initialize 2 quantum registers of size n
+    n = math.ceil(math.log(N)/math.log(2))
+    qc = QuantumCircuit(2*n, 2*n)
     
     #Change second register to state |1>
-    for x in range:
-        qc.x(n+x)
+    qc.x(2*n-1)
     
     #Add H gate to first register
-    for x in range(n):
-        qc.h(x)
+    for i in range(n):
+        qc.h(i)
+    
+    # Apply U_(a^x) here
+
+    qc.append(QFT(n), [i for i in range(n)])
+
+    for i in range(n):
+        qc.measure(i, i)
+    
+    print(qc.draw(output="text"))
     
     return 0
+
+oracle(2, 2, 8)
 
 # Solves the discrete logarithm problem for 
 # b = a^m (mod N) using repeated calls to the
