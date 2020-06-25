c876c51174a30d77b918708c1a15a9b58aebb691
Matthew <mattyg@live.unc.edu>
2020-06-25 10:45:34 -0400

    Added sketch of the algorithm

---
+++ algorithm.py
@@ -0,0 +1,23 @@
+from qiskit import *
+
+# In our final Jupyter notebook we won't have to
+# break things up functionally like this, but it
+# should help for now.
+
+# Calculates the multiplicative inverse of x mod N
+# (the number y such that xy = 1 (mod N)) using
+# the extended Euclidean algorithm.
+def invert(x, N):
+    return 1
+
+# b is some power of a, and the oracle outputs m,
+# where b = a^m (mod N) with >50% probability.
+# (this is where our main algorithm goes)
+def oracle(a, b, N):
+    return 0
+
+# Solves the discrete logarithm problem for 
+# b = a^m (mod N) using repeated calls to the
+# oracle defined above.
+def logarithm(a, b, N):
+    return 0
